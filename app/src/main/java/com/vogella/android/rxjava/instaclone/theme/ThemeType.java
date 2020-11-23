76e25f13566455edb162e1f6b6da89bfda32809b
Faruq Hammed <faruqhammed8@gmail.com>
2020-11-23 00:21:30 -0600

    fixed keys

--- app/src/main/java/com/vogella/android/rxjava/instaclone/theme/ThemeType.java
+++ app/src/main/java/com/vogella/android/rxjava/instaclone/theme/ThemeType.java
@@ -1,40 +1,4 @@
 package com.vogella.android.rxjava.instaclone.theme;
 
-import androidx.annotation.NonNull;
-
-public enum ThemeType {
-
-    LightMode{
-        @NonNull
-        public String toString() {
-            return "LightMode";
-        }
-    },
-    DarkMode{
-        @NonNull
-        public String toString() {
-            return "DarkMode";
-        }
-    },
-    DefaultMode{
-        @NonNull
-        public String toString() {
-            return "DefaultMode";
-        }
-    };
-
-    public static ThemeType getThemeType(String themeStr){
-        ThemeType themeType;
-        switch (themeStr) {
-            case "LightMode":
-                themeType = LightMode;
-                break;
-            case "DarkMode":
-                themeType = DarkMode;
-                break;
-            default:
-                themeType = DefaultMode;
-        }
-        return themeType;
-    }
+public class ThemeType {
 }
